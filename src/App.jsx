2d8dba9717a4a6c6c0738bc86fd69384b8ce2f5c
Janhvi Mahangade <janhvimmahangade@gmail.com>
2026-02-14 21:33:24 +0530

    Initial commit - Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./css/App.css";
+import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favorite from "./pages/Favorite";
+import Navbar from "./components/Navbar";
+import { MovieProvider } from "./contexts/MovieContext";
+import MovieContext from "./contexts/MovieContext";
+function App() {
+  return (
+    <MovieProvider>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Favorite" element={<Favorite />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
